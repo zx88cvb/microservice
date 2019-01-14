1313ad823b6835c029aca9c0dcc10199b17ebcea
Angel <295389754@qq.com>
2019-01-14 17:03:05 +0800

    py message

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # microservice
 Docker K8S 学习
+
+# 用户服务
+- 用户登录
+- 用户注册
+- 用户基本信息查询
+- 无状态 无session
+- 单点登录
+
+# 课程服务
+- 登录验证
+- 课程的crud
+
+# 信息服务
+- 发送邮件
+- 发送短信
+
+# 用户edgeservice
+# 课程edgeservice
+# API GETEWAY
---
+++ message-thrift-python-service/message/message_service.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+
+from message.api import MessageService
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+sender = 'zx88cvb@163.com'
+authCode = 'Xiang19951101'
+class MessageServiceHandler:
+    def sendMobileMessage(self, mobile, message):
+        """
+        Parameters:
+         - mobile
+         - message
+        """
+        print("sendMobileMessage, mobile: %s, message:%s", mobile, message)
+        pass
+
+    def sendEmailMessage(self, email, message):
+        """
+        Parameters:
+         - email
+         - message
+        """
+        print("sendEmailMessage, email: %s , message: %s", email, message)
+        messageObj = MIMEText(message, "plain", "utf-8")
+        messageObj['From'] = sender
+        messageObj['To'] = email
+        messageObj['Subject'] = Header("测试邮件", "utf-8")
+        try:
+            smtpObj = smtplib.SMTP("smtp.163.com")
+            smtpObj.login(sender, authCode)
+            smtpObj.sendmail(sender, [email], messageObj.as_string())
+            print("send mail success")
+            return True
+        except smtplib.SMTPException as ex:
+            print(ex)
+            return False
+
+
+if __name__ == "__main__":
+    handler = MessageServiceHandler()
+    handler.sendEmailMessage("295389754@qq.com", "哈哈啊")
+    processor = MessageService.Processor(handler)
+    transport = TSocket.TServerSocket('localhost', 9090)
+    tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+
+    print("starting thrift server in python")
+    server.serve()
+    print("done!")
