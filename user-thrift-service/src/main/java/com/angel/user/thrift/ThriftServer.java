73344320bbb2739360c7469d23a99b31bcde3e51
Angel <295389754@qq.com>
2019-01-15 16:36:05 +0800

    用户服务 thrift

--- user-thrift-service/src/main/java/com/angel/user/thrift/ThriftServer.java
+++ user-thrift-service/src/main/java/com/angel/user/thrift/ThriftServer.java
@@ -2,6 +2,12 @@ package com.angel.user.thrift;
 
 import com.angel.thrift.user.UserService;
 import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TNonblockingServer;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TTransportException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,8 +27,26 @@ public class ThriftServer {
     @Resource
     private UserService.Iface usreService;
 
+    /**
+     * 服务启动仅执行一次  相当于init()
+     */
     @PostConstruct
     public void startThriftServer() {
         TProcessor tProcessor = new UserService.Processor<>(usreService);
+
+        TNonblockingServerSocket socket = null;
+        try {
+            socket = new TNonblockingServerSocket(servicePort);
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+        TNonblockingServer.Args args =  new TNonblockingServer.Args(socket);
+        args.processor(tProcessor);
+        args.transportFactory(new TFramedTransport.Factory());
+        args.protocolFactory(new TBinaryProtocol.Factory());
+
+        TServer server = new TNonblockingServer(args);
+
+        server.serve();
     }
 }
