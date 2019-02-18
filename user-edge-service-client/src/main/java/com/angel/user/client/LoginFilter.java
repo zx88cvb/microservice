8f11cc1e19363a5cbd64f00f872d74060dd313af
zx88cvb <295389754@qq.com>
2019-02-18 22:36:52 +0800

    docker-compose

--- user-edge-service-client/src/main/java/com/angel/user/client/LoginFilter.java
+++ user-edge-service-client/src/main/java/com/angel/user/client/LoginFilter.java
@@ -72,7 +72,7 @@ public abstract class LoginFilter implements Filter {
     protected abstract void login(HttpServletRequest request, HttpServletResponse response, UserDTO userDTO);
 
     private UserDTO requestUserInfo(String token) {
-        String url = "http://127.0.0.1:8080/user/authentication";
+        String url = "http://user-edge-service:8082/user/authentication";
         HttpClient httpClient = HttpClientBuilder.create().build();
         HttpPost post = new HttpPost(url);
         post.addHeader("token", token);
